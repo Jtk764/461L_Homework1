ced10755045b04a0b70e4a59e0649b6ecd2529d8
Josh Knight <Josh Knight>
2017-02-11 15:06:47 -0600

    more starter

--- src/blogSpot/Blog.java
+++ src/blogSpot/Blog.java
@@ -1,5 +1,35 @@
 package blogSpot;
+import java.util.Date;
+import com.google.appengine.api.users.User;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 
-public class Blog {
 
+@Entity
+public class Blog implements Comparable<Blog> {
+    @Id Long id;
+    User user;
+    String content;
+    Date date;
+    private Blog() {}
+    public Blog(User user, String content) {
+        this.user = user;
+        this.content = content;
+        date = new Date();
+    }
+    public User getUser() {
+        return user;
+    }
+    public String getContent() {
+        return content;
+    }
+    @Override
+    public int compareTo(Blog other) {
+        if (date.after(other.date)) {
+            return 1;
+        } else if (date.before(other.date)) {
+            return -1;
+        }
+        return 0;
+    }
 }
--- src/blogSpot/BlogSpotServlet.java
+++ src/blogSpot/BlogSpotServlet.java
@@ -1,12 +1,44 @@
-package blogSpot;
+import com.google.appengine.api.datastore.DatastoreService;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+import com.google.appengine.api.datastore.Entity;
+
+import com.google.appengine.api.datastore.Key;
+
+import com.google.appengine.api.datastore.KeyFactory;
+
+import com.google.appengine.api.users.User;
+
+import com.google.appengine.api.users.UserService;
+
+import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
 
 import java.io.IOException;
-import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
 public class BlogSpotServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		ObjectifyService.register(Blog.class);
+		String content = req.getParameter("content");
+		Blog blog = new Blog(user, content);
+		ofy().save().entity(blog).now();
+		resp.sendRedirect("ofyguestbook.jsp?");
+
 	}
+
 }
